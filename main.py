fb883ff397bebe178a2defa6ecdcfc4c34b27bdb
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-01-22 22:11:38 +0000

    Initialise pygame and create game window

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+
+# Init pygame
+pygame.init()
+
+# Create window
+window = pygame.display.set_mode((800, 600))
+
+# Game loop
+is_running = True
+while is_running:
+    for event in pygame.event.get():
+        # End the program and close the window if 'X' window decoration is clicked
+        if event.type == pygame.QUIT:
+            is_running = False
